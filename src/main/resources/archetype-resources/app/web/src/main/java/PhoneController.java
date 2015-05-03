6f709e2b9013867c1b433b7136738677bd673cd4
EdwardsBean <EdwardsBean@gmail.com>
2015-05-03 11:57:47 +0800

    add version route support in xml for config

--- src/main/resources/archetype-resources/app/web/src/main/java/PhoneController.java
+++ src/main/resources/archetype-resources/app/web/src/main/java/PhoneController.java
@@ -14,7 +14,7 @@ public class PhoneController {
     @Autowired
     PhoneService phoneService;
 
-    @ApiOperation(value = "查看手机日志", httpMethod = "GET", response = Msg.class)
+    @ApiOperation(value = "查看手机日志")
     @RequestMapping(value = "/phones/{id}", method = RequestMethod.GET)
     public Msg get(@ApiParam(required = true, value = "手机日志id") @PathVariable long id) {
         Msg msg = new Msg();
@@ -22,7 +22,7 @@ public class PhoneController {
         return msg;
     }
 
-    @ApiOperation(value = "上传手机日志", httpMethod = "POST", response = Msg.class)
+    @ApiOperation(value = "上传手机日志")
     @RequestMapping(value = "/phones", method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE)
     public Msg post(@ApiParam(required = true,name = "content", value = "手机日志内容") @RequestParam String content) {
         Phone phone = new Phone();
@@ -33,7 +33,7 @@ public class PhoneController {
         return msg;
     }
 	
-	@ApiOperation(value = "上传JSON日志", httpMethod = "POST", response = Msg.class, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(value = "上传JSON日志",consumes = MediaType.APPLICATION_JSON_VALUE)
     @RequestMapping(value = "/phones/logs", produces = MediaType.APPLICATION_JSON_VALUE)
     public Msg upload(@RequestBody Phone phone) {
         Msg msg = new Msg();
